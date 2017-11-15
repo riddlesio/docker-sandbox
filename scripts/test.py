77af6b60891a4a65b7c6c48e2d9b4c404eebdaf8
Niko van Meurs <nikovanmeurs@gmail.com>
2017-11-15 14:51:50 +0100

    feat: Test should exit with return code from the subprocess [t:5m]

--- scripts/test.py
+++ scripts/test.py
@@ -2,4 +2,6 @@ from test.subprocess import SubprocessRunner
 
 if __name__ == '__main__':
     args = sys.argv[1::]
-    SubprocessRunner().run('pytest {}'.format(' '.join(args)))
+    command = 'pytest' if not len(args) else 'pytest -m "{}"'.format(' '.join(args))
+    result = SubprocessRunner().run(command)
+    exit(result.return_code)
