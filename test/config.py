5d808431e1268bf7ee41661a5cfbd7840cf5d1f0
Niko van Meurs <nikovanmeurs@gmail.com>
2017-11-17 15:33:32 +0100

    chore: Enable C# [t:5m]

--- test/config.py
+++ test/config.py
@@ -10,7 +10,7 @@ and the runtime which should execute the binary.
 '''
 
 PROGRAMMING_LANGUAGES = [
-    # ('cs', None),
+    ('cs', 'mono'),
     ('haskell', None),
     ('java', 'java -jar'),
     ('javascript', 'node'),
